958e954e40e26fd6ebc5e12d5a16e4b4c1b98060
Hrishikesh Tarale <86829991+hrishitarale@users.noreply.github.com>
2026-04-15 16:10:52 +0530

    bug fix in App.js

--- lending-system/frontend/src/App.js
+++ lending-system/frontend/src/App.js
@@ -1,7 +1,7 @@
-const BASE_URL = process.env.REACT_APP_API_URL;
-
 import React, { useState } from "react";
 import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   const [form, setForm] = useState({
